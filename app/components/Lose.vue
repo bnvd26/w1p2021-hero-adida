452340ad83323bdd0a83b4aa56a9a52a504db00f
benads <benjaminad5@yahoo.com>
2019-05-24 16:41:59 +0200

    tst prod

--- app/components/Lose.vue
+++ app/components/Lose.vue
@@ -6,17 +6,25 @@
 		appear>
   <div class="big-header page">
     <div class="center"></div>
-   <h1>{{message}}</h1>
-   <p>Malheursement l'aventure s'arrete ici avec {{perso}}</p>
-   <div v-if="perso == 'kombouare'" style="display: flex;flex-direction: column;align-items: center;">
+   <h1 style="font-size: 40px; color:#14133f;border:2px solid black; background-color: white;">{{message}}</h1>
+   <p style="font-size: 40px;border:2px solid black; background-color: white;color:#14133f">Malheursement l'aventure s'arrete ici avec {{perso}}</p>
+   <div v-if="perso == 'kombouare'" style="display: flex;flex-direction: column;align-items: center; color:#14133f; border:2px solid black;background-color: white;text-align: center;">
      <img src="../assets/images/relgation.jpeg" style="position: fixed; min-width: 100%; min-height:100%; z-index:-1; bottom:0; left:0;">
      <p style="font-size: 40px;">Cest la furie dans le stade </p>
-     <p>Votre equipe est releguee en deuxieme divison vous avez certainement pas fait les bons choix</p>
+     <p style="font-size: 40px;">Votre equipe est releguee en deuxieme divison vous avez certainement pas fait les bons choix</p>
+               <p>Vous terminez avec {{pointss}} points de reputation et {{mentale}} points de mental</p>
    </div>
-     <div v-else-if="perso == 'klopp'" style="display: flex;flex-direction: column;align-items: center;">
+     <div v-else-if="perso == 'klopp'" style="display: flex;flex-direction: column;align-items: center; color:#14133f; border:2px solid black;background-color: white;text-align: center;">
      <img src="../assets/images/deception.jpg" style="position: fixed; min-width: 100%; min-height:100%; z-index:-1; bottom:0; left:0;">
      <p style="font-size: 40px;">QUELLE DECEPTION POUR LES JOUEURS ET LES SUPPORTERS</p>
-     <p>Votre equipe a perdue la finale vous avez certainement pas fait les bons choix</p>
+     <p style="font-size: 40px;">Votre equipe a perdue la finale vous avez certainement pas fait les bons choix</p>
+               <p>Vous terminez avec {{pointss}} points de reputation et {{mentale}} points de mental</p>
+   </div>
+      <div v-else-if="perso == 'zidane'" style="display: flex;flex-direction: column;align-items: center; color:#14133f; border:2px solid black;background-color: white;text-align: center;">
+     <img src="../assets/images/deception.jpg" style="position: fixed; min-width: 100%; min-height:100%; z-index:-1; bottom:0; left:0;">
+     <p style="font-size: 40px;">QUELLE DECEPTION POUR LES JOUEURS ET LES SUPPORTERS</p>
+     <p style="font-size: 40px;">Votre equipe a perdue la finale vous avez certainement pas fait les bons choix</p>
+            <p>Vous terminez avec {{pointss}} points de reputation et {{mentale}} points de mental</p>
    </div>
     <router-link class="button" to="/home">Go to Home</router-link>
     <router-link class="button" @click.prevent="clearScore()" to="/character">PLAY AGAIN</router-link>
@@ -29,7 +37,10 @@ export default {
     data() {
       return {
         points: gameService.value(),
+        pointss: localStorage.getItem('points'),
         perso: localStorage.getItem('character'),
+        mentale : localStorage.getItem('Mentalite'),
+        
         message : "VOUS AVEZ PERDU !!",
         komb: require('../assets/images/relgation.jpeg')
 
--- app/components/Win.vue
+++ app/components/Win.vue
@@ -2,16 +2,25 @@
 <transition v-on:enter="enter" v-on:leave="leave" v-bind:css="false" appear>
   <div class="big-header page">
     <div class="center"></div>
-    <h1 style="font-size: 40px">{{message}}</h1>
-     <div v-if="perso == 'kombouare'" style="display: flex;flex-direction: column;align-items: center;">
-     <img src="../assets/images/relgation.jpeg" style="position: fixed; min-width: 100%; min-height:100%; z-index:-1; bottom:0; left:0;">
-     <p style="font-size: 40px;">Cest la furie dans le stade </p>
-     <p>Votre equipe est releguee en deuxieme divison vous avez certainement pas fait les bons choix</p>
+    <h1 style="font-size: 40px; color:#14133f;border:2px solid black; background-color: white;">{{message}}</h1>
+     <div v-if="perso == 'kombouare'" style="display: flex;flex-direction: column;align-items: center; color:#14133f; border:2px solid black;background-color: white;text-align: center;">
+     <img src="../assets/images/maintien.jpg" style="position: fixed; min-width: 100%; min-height:100%; z-index:-1; bottom:0; left:0;">
+     <p style="font-size: 40px;">QUELLE FIN DE SAISON ET QUELLE JOIE POUR CETTE EQUIPE</p>
+     <p>Votre equipe est maintenue en premiere divison.</p>
+       <p>Vous terminez avec {{reput}} points de reputation et {{mentale}} points de mental</p>
    </div>
-     <div v-else-if="perso == 'klopp'" style="display: flex;flex-direction: column;align-items: center;">
-     <img src="../assets/images/deception.jpg" style="position: fixed; min-width: 100%; min-height:100%; z-index:-1; bottom:0; left:0;">
-     <p style="font-size: 40px;">QUELLE DECEPTION POUR LES JOUEURS ET LES SUPPORTERS</p>
-     <p>Votre equipe a perdue la finale vous avez certainement pas fait les bons choix</p>
+     <div v-else-if="perso == 'klopp'" style="display: flex;flex-direction: column;align-items: center; color:#14133f; border:2px solid black; background-color: white;text-align: center;">
+     <img src="../assets/images/winners.jpg" style="position: fixed; min-width: 100%; min-height:100%; z-index:-1; bottom:0; left:0;">
+     <p style="font-size: 40px;">QUELLE JOIE POUR LES JOUEURS ET LES SUPPORTERS</p>
+     <p style="font-size: 40px;">Votre equipe a gagner la final  </p>
+    <p>Vous terminez avec {{reput}} points de reputation et {{mentale}} points de mental</p>
+   </div>
+     <div v-else-if="perso == 'zidane'" style="display: flex;flex-direction: column;align-items: center; color:#14133f; border:2px solid black; background-color: white;text-align: center;">
+     <img src="../assets/images/winners.jpg" style="position: fixed; min-width: 100%; min-height:100%; z-index:-1; bottom:0; left:0;">
+     <p style="font-size: 40px;">QUELLE JOIE POUR LES JOUEURS ET LES SUPPORTERS</p>
+     <p style="font-size: 40px;">Votre equipe a gagner la final  </p>  
+            <p>Vous terminez avec {{reput}} points de reputation et {{mentale}} points de mental</p>
+
    </div>
     <router-link class="button" to="/home">Go to Home</router-link>
   </div>
@@ -24,7 +33,9 @@ export default {
       return {
         message : "LA VICTOIRE",
         audio: false,
-        perso: localStorage.getItem('character')
+        perso: localStorage.getItem('character'),
+        mentale : localStorage.getItem('Mentalite'),
+        reput : localStorage.getItem('points')
       }  
     },
     methods: {
